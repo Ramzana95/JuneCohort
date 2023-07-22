557b135d470cff8bff58ebd99883e9de65d5939b
Ramzan Ahmad <Ramzan@Ramzans-MacBook-Pro.local>
2023-07-22 01:42:08 +0100

    Uploading Javascript work

---
+++ Javascript/homework1.js
@@ -0,0 +1,45 @@
+
+console.log("Hello, this is my first homework for Javascript.")
+ //q1. Using string concatenation add two different strings together and print this off.
+ console.log("Hello"+"world!")
+ /*This prints the strings without a space in between. to create a space, you must put a space between 
+ the first word and the end of the quotation mark, or you can 
+ put a space between the quotation mark and the start of the second word. e.g.*/
+console.log("Hello " + "world!") //or
+console.log("Hello" + " world!") //or
+console.log("Hello" + " " + "world!")
+
+/*Another way of creating a space between two strings is by creating variables e.g.*/
+var firstWord = "Hello"
+var secondWord = "world!"
+console.log(firstWord + " " + secondWord)
+
+
+//q2. Using variables, create a variable for a number and a string, then print each of these variables.
+var firstWord = "Hello"
+var secondWord = 2
+console.log(firstWord + " " + secondWord)
+console.log(firstWord)
+console.log(secondWord)
+
+/* Using string concatenation in JS, combine 3 different variables and print them off.
+i.e. a variable for name, age, and DOB will result in "my name is Ramzan I am 28 years
+old and I was born in 1995". (You cannot use name, age, and DOB as your variables.)*/
+
+var name = "Ramzan"
+var age = 28
+var dob = 1995
+console.log("my name is", name,  "I am", age,
+"years old and I was born in", dob)
+
+
+
+
+
+
+
+
+
+
+ 
+ 
---
+++ Javascript/lesson1.js
@@ -0,0 +1,22 @@
+//this is a single line comment, only for the user to read. Not for the computer to read
+/*This 
+is
+a 
+multi line
+comment*/
+
+console.log("Hello you have executed the lesson 1 file")
+
+console.log("Hello world!");
+
+var myName = "Ramzan";
+
+console.log (myName);
+
+var numberOne = "1";
+
+console.log(numberOne)
+
+console.log(123);
+
+
